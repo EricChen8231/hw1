26cc9ae7eeb4180b563737d24fa7aa1f60ba8e8a
Eric Chen <echen823@usc.edu>
2024-01-25 20:07:02 -0800

    done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -2,12 +2,30 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
-
 #include "ulliststr.h"
+#include <cassert>
+int main() {
+  // Your ULListStr class and methods should be declared and defined here
 
-//Use this file to test your ulliststr implementation before running the test suite
+  ULListStr myLinkedList;
 
-int main(int argc, char* argv[])
-{
+  // Add and remove an element 5 times
+  for (int i = 0; i < 5; ++i) {
+    std::string elementToAdd = "Element" + std::to_string(i);
 
+    // Add element to the back of the list
+    myLinkedList.push_back(elementToAdd);
+    std::cout << "Added: " << elementToAdd << std::endl;
+
+    // Remove element from the front of the list
+    if (!myLinkedList.empty()) {
+      std::string removedElement = myLinkedList.front();
+      myLinkedList.pop_front();
+      std::cout << "Removed: " << removedElement << std::endl;
+    } else {
+      std::cout << "List is empty. Cannot remove." << std::endl;
+    }
+  }
+
+  return 0;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -1,6 +1,7 @@
 #include <cstddef>
 #include <stdexcept>
 #include "ulliststr.h"
+#include <iostream>
 
 ULListStr::ULListStr()
 {
@@ -55,11 +56,174 @@ std::string const & ULListStr::get(size_t loc) const
 
 void ULListStr::clear()
 {
-  while(head_ != NULL){
-    Item *temp = head_->next;
-    delete head_;
-    head_ = temp;
+  Item *current = head_;
+  while (current != nullptr)
+  {
+    Item *next = current->next;
+    delete current;
+    current = next;
   }
-  tail_ = NULL;
+  head_ = tail_ = nullptr;
   size_ = 0;
 }
+
+void ULListStr::push_back(const std::string &val)
+{
+  if (empty())
+  {
+    head_ = new Item();
+    tail_ = head_;
+  }
+
+  if (tail_->last == ARRSIZE) 
+  {
+    Item *newItem = new Item();
+    newItem->prev = tail_;
+    tail_->next = newItem;
+    tail_ = newItem;
+  }
+
+  tail_->val[tail_->last] = val;
+  tail_->last++;
+  tail_->next = nullptr;
+  size_++;
+
+}
+void ULListStr::push_front(const std::string &val)
+{
+  if (empty())
+  {
+    head_ = new Item();
+    tail_ = head_;
+  }
+
+  if (head_->first > 0)
+  {
+    head_->first--;
+    head_->val[head_->first] = val;
+  }
+  else
+  {
+    Item *newItem = new Item();
+    newItem->next = head_;
+    head_->prev = newItem;
+    head_ = newItem;
+    head_->first = ARRSIZE - 1;
+    head_->last = ARRSIZE;
+    head_->val[head_->first] = val;
+  }
+  size_++;
+}
+
+void ULListStr::pop_front()
+{
+  if (empty())
+    return;
+
+  if (head_->first < ARRSIZE - 1)
+  {
+    head_->first++;
+  }
+  else
+  {
+    Item *temp = head_;
+    head_ = head_->next;
+    if (head_ != nullptr)
+    {
+      head_->prev = nullptr;
+      head_->first = 0;
+    }
+    else
+    {
+      tail_ = nullptr;
+    }
+
+    delete temp;
+  }
+
+  size_--;
+  if (empty())
+  {
+    clear();
+  }
+}
+void ULListStr::pop_back()
+{
+  if (empty())
+    return;
+
+  if (tail_->last > 0)
+  {
+  
+    size_--;
+    tail_->last--;
+  }
+  else
+  {
+    Item *temp = tail_;
+    tail_ = tail_->prev;
+
+    if (tail_ != nullptr)
+    {
+      tail_->last = ARRSIZE - 1; 
+      tail_->next = nullptr;
+    }
+    else
+    {
+      head_ = nullptr;
+    }
+    delete temp;
+    size_--;
+  }
+  if (empty())
+  {
+    clear(); 
+  }
+}
+
+std::string const &ULListStr::back() const
+{
+  if (empty())
+  {
+    throw std::out_of_range("List is empty");
+  }
+
+  if (tail_->last == 0)
+  {
+    if (tail_->prev != nullptr)
+    {
+      return tail_->prev->val[ARRSIZE - 1];
+    }
+    else
+    {
+      throw std::out_of_range("List is empty");
+    }
+  }
+  return tail_->val[tail_->last - 1];
+}
+
+std::string const &ULListStr::front() const
+{
+  if (empty())
+  {
+    throw std::out_of_range("List is empty");
+  }
+  return head_->val[head_->first];
+}
+std::string *ULListStr::getValAtLoc(size_t loc) const
+{
+  size_t currentPos = 0;
+  Item *current = head_;
+  while (current != nullptr && currentPos + (current->last - current->first) <= loc)
+  {
+    currentPos += (current->last - current->first);
+    current = current->next;
+  }
+  if (current == nullptr || loc - currentPos >= current->last - current->first)
+  {
+    return nullptr;
+  }
+  size_t indexWithinNode = loc - currentPos + current->first;
+  return &(current->val[indexWithinNode]);
+}
+
